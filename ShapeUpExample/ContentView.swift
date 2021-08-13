e98a0ff6ae74bfc2f16cadfd011186613a84345c
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2021-08-13 15:57:17 -0400

    Initial Commit

---
+++ ShapeUpExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShapeUpExample
+//
+//  Created by Ryan Lintott on 2021-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
